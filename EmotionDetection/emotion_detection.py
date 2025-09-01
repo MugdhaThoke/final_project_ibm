a9eaedada5499a8b0d1f0a1ef58fb702e94b29dc
Mugdha Thoke <mugdhathoke0909@gmail.com>
2025-09-01 15:20:23 -0400

    Initial Commit

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,24 @@
+import requests 
+import json
+def emotion_detector(text_to_analyse):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = { "raw_document": { "text": text_to_analyse } }
+    response = requests.post(url, json = myobj, headers = header)
+    formatted_response = json.loads(response.text)
+    prediction = formatted_response['emotionPredictions'][0]['emotion']
+    anger_score = prediction['anger']
+    disgust_score = prediction['disgust']
+    fear_score = prediction['fear']
+    joy_score = prediction['joy']
+    sadness_score = prediction['sadness']
+    emotion_scores = {
+            'anger': anger_score, 'disgust': disgust_score,'fear': fear_score,
+            'joy': joy_score,'sadness': sadness_score
+        }
+    dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+    return {'anger': anger_score,'disgust': disgust_score,'fear': fear_score,
+            'joy': joy_score, 'sadness': sadness_score, 'dominant_emotion': dominant_emotion
+            }
+
+ 
